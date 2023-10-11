fd17ea3c9ecd54f248c7de3a1a3422e877df4c4b
peak2 <36886305+peak2@users.noreply.github.com>
2023-10-11 01:37:34 +0100

    trach

---
+++ src/component/Header.tsx
@@ -0,0 +1,32 @@
+import { Button } from "./Button"
+import { useLocation } from "react-router-dom"
+interface HeaderType {
+    title?: string
+    onAdd: () => void
+    showTask: boolean
+
+}
+const color = {
+    backgroundCor: "red"
+}
+
+const Header: React.FC<HeaderType> = ({ title, onAdd, showTask }) => {
+    const location = useLocation()
+    return (
+        <header className="header">
+            {<h1>{title}</h1>}
+
+
+            {location.pathname === '/e' && (<Button className={`${showTask ? color : " "}  btn`}
+                text={showTask ? "Close" : "Add"}
+                backgroundColor={showTask ? "red" : "green"}
+                onClick={onAdd} />)}
+        </header>
+    )
+}
+Header.defaultProps = {
+    title: "Task Tracker"
+
+}
+
+export default Header
